43c95efc55278c82421b436abf3e8473a8424f34
iagocity <93496367+iagocity@users.noreply.github.com>
2022-03-10 16:23:57 +0100

    Add files via upload

---
+++ aulavirtual-publico.ps1
@@ -0,0 +1,21 @@
+Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/login/index.php
+
+(
+  $login = (Read-Host -Prompt 'Escribe Y cuando hayas iniciado sesion. (Y/N)') -eq 'y'
+)
+
+if ($login)
+{
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=138
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=539
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=159
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=239
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=294
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=279
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=555
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=104
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=643
+  Start https://www.edu.xunta.gal/centros/iesagraorzan/aulavirtual/course/view.php?id=211
+  Start https://bit.ly/damedecomer
+  Write-Host "Dame de comer"
+}
